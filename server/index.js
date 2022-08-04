47b0e1f14ba4a0c0594d5ea77e9d578bf031c330
Andrew Tapper <andrew@tapper.codes>
2022-08-03 23:38:49 -0500

    basic create post logic

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import 'dotenv/config'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
