824c30ab4a04b0d54c417daf6f2d545a1bb581b8
Andrew Tapper <andrew@tapper.codes>
2022-08-05 22:23:14 -0500

    add custom svg background to client index

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
         <Container>
           <Grid
             container
-            justify="space-between"
+            justifyContent="space-between"
             alignItems="stretch"
             spacing={3}>
             <Grid item xs={12} sm={7}>
