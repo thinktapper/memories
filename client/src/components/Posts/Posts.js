21c1703c5d2dec51472bf1ff2cec2cfd74cb27c8
Andrew Tapper <andrew@tapper.codes>
2022-08-05 21:49:55 -0500

    memories can be created using the form, and posts are now displayed in the app

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import Post from './Post/Post'
 import useStyles from './styles'
@@ -8,10 +9,15 @@ const Posts = () => {
   const classes = useStyles()
 
   return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-    </>
+      !posts.length ? <CircularProgress /> : (
+        <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+          {posts.map(post => (
+            <Grid item key={post._id} xs={12} sm={6} md={4}>
+              <Post post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      )
   )
 }
 
